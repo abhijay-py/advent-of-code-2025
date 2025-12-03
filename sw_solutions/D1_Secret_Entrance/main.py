4575c3d526030cf4cebfb9c714b18eec3722409d
abhijay-py <jay62831@gmail.com>
2025-12-02 22:26:14 -0500

    Cleaned up day 1 and finished day 2 pt 1

--- sw_solutions/D1_Secret_Entrance/main.py
+++ sw_solutions/D1_Secret_Entrance/main.py
@@ -1,28 +1,37 @@
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+direction_to_value = {'L': -1, 'R': 1}
+
+def get_data():
+    with open(os.path.join(here, "input.txt")) as f:
+        data = f.readlines()
+    return data
+
 def part_one():
     current_pos = 50
-    direction_to_value = {'L': -1, 'R': 1}
     zero_count = 0
-    with open("sw_solution/D1_Secret_Entrance/input.txt") as f:
+    with open(os.path.join(here, "input.txt")) as f:
         data = f.readlines()
     for line in data:
         current_pos += direction_to_value[line[0]] * int(line[1:]) 
         current_pos %= 100
         if current_pos == 0:
             zero_count += 1
+            
     print(f"Initial Password: {zero_count}")
 
 def part_two():
     current_pos = 50
-    direction_to_value = {'L': -1, 'R': 1}
     zero_count = 0
-    with open("sw_solution/D1_Secret_Entrance/input.txt") as f:
-        data = f.readlines()
+    data = get_data()
     for line in data:
         old_pos = current_pos
         full_rotations = int(line[1:]) // 100
         current_pos += direction_to_value[line[0]] * (int(line[1:]) - (100*full_rotations))
         zero_count += ((current_pos > 99 and line[0] == 'R') or (current_pos < 0 and line[0] == 'L' and old_pos != 0) or (current_pos == 0 and current_pos != old_pos)) + full_rotations 
         current_pos %= 100
+
     print(f"New Password: {zero_count}")
 
 def main():
--- sw_solutions/D2_Gift_Shop/main.py
+++ sw_solutions/D2_Gift_Shop/main.py
@@ -1,11 +1,58 @@
-def main():
-    with open("sw_solution/D2_Gift_Shop/input.txt") as f:
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+def get_pairs():
+    with open(os.path.join(here, "input.txt")) as f:
         data = f.readline().strip().split(',')
     
-    pairs = [(int(j) for j in entry.split('-')) for entry in data]
-    temp = [len(entry.split('-')[0]) for entry in data]
-    pair_len = [i / 2 if i / 2.0 == i // 2 else -1 for i in temp]
-    
+    pairs = [(j for j in entry.split('-')) for entry in data]
 
+    return pairs
+
+def part_one():
+    pairs = get_pairs()
+    invalid_id_sum = 0
+    
+    for i, j in pairs:
+        lower_bound = int(i)
+        upper_bound = int(j)
+        lower_digit_count = int(len(i) // 2)
+        upper_digit_count = int(len(j) // 2)
+        
+        if len(i) == len(j) and len(i) % 2 != 0:
+            continue
+        elif len(i) != len(j):
+            if len(i) % 2 != 0:
+                lower_bound = 10 ** (len(i))
+                lower_digit_count = int((len(i) + 1) // 2)
+            if len(j) % 2 != 0:
+                upper_bound = 10 ** (len(j) - 1) - 1
+                upper_digit_count = int((len(j) - 1) // 2)
+        
+        for seq_len in range(lower_digit_count, upper_digit_count + 1):
+            factor = (10 ** seq_len + 1)
+            current_lower = lower_bound
+            current_upper = upper_bound
+            if 10 ** (seq_len * 2 - 1) > lower_bound:
+                current_lower = 10 ** (seq_len * 2 - 1) 
+            if 10 ** (seq_len * 2) - 1 < upper_bound:
+                current_upper = 10 ** (seq_len * 2) - 1
+            lower_multiple = current_lower // factor
+            upper_multiple = current_upper // factor 
+
+            for m in range(lower_multiple, upper_multiple + 1):
+                value = m * factor
+                if value >= current_lower and value <= current_upper:
+                    invalid_id_sum += value
+
+    print(f"Sum of Invalid Gift IDs: {invalid_id_sum}")
+
+def part_two():
+    pass
+
+def main():
+    part_one()
+    
 if __name__ == "__main__":
     main()
